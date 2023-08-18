e42e88739e4c17fc9c68e2103442ccf3bf1bf146
Danya39 <danyazaky@gmail.com>
2023-08-18 14:08:14 -0700

    a

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -4,7 +4,35 @@
 #include <ctype.h>
 #include <string.h>
 
+
+/**
+ * _strncat - function concatenate two strings
+ * using at most n bytes from src
+ * @dest: entered value
+ * @src: entered value
+ * @n: entered value
+ *
+ * Return: dest
+ */
+
+
 char *_strncat(char *dest, char *src, int n)
 {
-	return strncat(dest, src, n);
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
 }
